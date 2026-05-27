adb3134c5ea36fd23640f8b4cb05e1089b07c6d0
amangupta <amangupta954055@gmail.com>
2026-05-28 01:30:19 +0530

    adding additional environmental variable and changing the wxt config port

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  dev: {
+    server: {
+      port: 3001,
+    },
+  },
   modules: ["@wxt-dev/module-react"],
   srcDir: "src",
   manifest: {
     name: "Link Crust",
     description: "Save and revisit links instantly without breaking your flow.",
-    permissions: ["storage", "tabs", "activeTab"],
+    permissions: ["storage", "tabs", "activeTab", "cookies"],
+    host_permissions: ["http://localhost:3000/*", "https://*.linksmasher.com/*"],
     action: {},
   },
 });
